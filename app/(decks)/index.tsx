136b0a2db6e08a7940bab1648f57ff65dde41263
Benjo <maglicbenjamin@gmail.com>
2025-06-24 18:48:33 +0200

    fetch decks from memowise-api

--- app/(decks)/index.tsx
+++ app/(decks)/index.tsx
@@ -1,13 +1,42 @@
-import { Link } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { Deck } from '@/models/deck';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
 
 export default function DecksScreen() {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState<Deck[]>([]);
+
+    const getDecks = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/decks');
+            const json = await response.json();
+            setData(json);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getDecks();
+    }, []);
+
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text>Decks screen</Text>
-            <Link href="/create" asChild>
-                <Button title={"Create deck"} />
-            </Link>
+        <View style={{ flex: 1, padding: 24 }}>
+            {isLoading ? (
+                <ActivityIndicator />
+            ) : (
+                <FlatList
+                    data={data}
+                    keyExtractor={({ id }) => id}
+                    renderItem={({ item }) => (
+                        <Text>
+                            {item.id} {item.name}
+                        </Text>
+                    )}
+                />
+            )}
         </View>
-    )
+    );
 }
